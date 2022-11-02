25c02803523fe9874954295d58f6450f10262c78
Cody J. Hanson <cody@meltano.com>
2022-11-02 15:36:23 -0500

    Logging statement to debug Windows

--- src/meltano/core/state_store/filesystem.py
+++ src/meltano/core/state_store/filesystem.py
@@ -481,10 +481,16 @@ class WindowsFilesystemStateStoreManager(LocalFilesystemStateStoreManager):
             state_id_path = state_id
         else:
             state_id_path = f"{self.delimiter}{state_id}"
-        return (
+        ret_val = (
             self.join_path(self.state_dir, state_id_path, filename)
             if filename
             else self.join_path(self.state_dir, state_id_path)
+        )
+        logging.info(f"get_path called with {state_id}, {filename}; returned {ret_val}")
+        return (
+            self.join_path(self.state_dir, state_id_path, filename)
+            if filename
+            else self.join_path(self.state_dir, state_id_pat)
         )
 
     def get_state_ids(self, pattern: str | None = None):
