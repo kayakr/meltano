bc5001c9de47a3ce60ceb172852344043200f8b0
Cody J. Hanson <cody@meltano.com>
2022-11-04 15:17:32 -0500

    Add integration tests

---
+++ integration/meltano-state-s3/validate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+LOG_FILE="./docs/example-library/meltano-run/integration-test.log"
+
+# Custom validations for the `meltano-run` guide/test.
+
+# test that meltano run saw dbt:test (set=1) as successfully completed
+grep "Block run completed.           block_type=InvokerCommand err=None set_number=1 success=True" $LOG_FILE
+# test that we see dbt:run output
+grep "Done. PASS=1 WARN=0 ERROR=0 SKIP=0 TOTAL=1 cmd_type=command name=dbt-postgres stdio=stderr" $LOG_FILE
+
+# we could also run psql statements
+# check for the existance of files
+# etc
+source "$(git rev-parse --show-toplevel)/integration/commons.sh"
+cd "${TEST_DOCS_DIR}"
+meltano state list | grep "dev:tap-gitlab-to-target-jsonl"
+meltano state get dev:tap-gitlab-to-target-jsonl | grep "singer_state"
+
+meltano state set dev:tap-gitlab-to-target-jsonl '{"singer_state": {"bookmark-1": 0}}'
+
+meltano state get dev:tap-gitlab-to-target-jsonl | grep '{"singer_state": {"bookmark-1": 0}}'
