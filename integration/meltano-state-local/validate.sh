fe2529a9a896fcfc6e6fba7af653cd3aba499a12
Cody J. Hanson <cody@meltano.com>
2022-11-04 16:09:33 -0500

    --force flag for meltano state set

--- integration/meltano-state-local/validate.sh
+++ integration/meltano-state-local/validate.sh
@@ -12,6 +12,6 @@ cd "${TEST_DOCS_DIR}"
 meltano state list | grep "dev:tap-gitlab-to-target-jsonl"
 meltano state get dev:tap-gitlab-to-target-jsonl | grep "singer_state"
 
-meltano state set dev:tap-gitlab-to-target-jsonl '{"singer_state": {"bookmark-1": 0}}'
+meltano state set --force dev:tap-gitlab-to-target-jsonl '{"singer_state": {"bookmark-1": 0}}'
 
 meltano state get dev:tap-gitlab-to-target-jsonl | grep '{"singer_state": {"bookmark-1": 0}}'
